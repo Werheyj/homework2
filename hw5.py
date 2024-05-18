1853d365ca704f73794c54d402613620acecc35e
Tacha <Anastasiykoshe@gmail.com>
2024-05-18 15:41:51 +0300

    Функции. Функции с параметром.

---
+++ hw5.py
@@ -0,0 +1,9 @@
+def print_params(name_and_last_name):
+    print('Name and last_name: ', name_and_last_name)
+    print('Name and last_name: ', name_and_last_name)
+
+
+print_params('John Watson')
+print_params('Sherlock Holmes')
+print_params('Lucy Maud Montgomery')
+
